442a5d59d6a0964aa4b225e8789d61b85a82dfcd
JuanVascoGiraldo <79484258+JuanVascoGiraldo@users.noreply.github.com>
2021-04-01 04:17:32 -0600

    hacer ejercicios 9 y 10 de JavaScrip

--- js/problema02.js
+++ js/problema02.js
@@ -11,7 +11,6 @@ function calcular(){
     var ventas3= parseFloat(document.problema2.venta3.value);
     var comi= (ventas1+ventas2+ventas3)*0.1;
     document.problema2.comicion.value = "$"+comi;
-
     var tota= parseFloat(document.problema2.sueldo.value)+ comi;
     document.problema2.total.value = "$"+tota;
 }
@@ -75,42 +74,107 @@ function validarnumero2(e){
 function operacionesnum(){
     var n1= parseFloat(document.problema7.num1.value);
     var n2= parseFloat(document.problema7.num2.value);
-    var re= 0;
-    var op= "";
-if(n1==n2){
-    re= n1*n2;
-    op="Multiplicación"
-}else{
-    if(n1>n2){
-        re= n1-n2;
-        op="Resta"
-    }
-    else{
-        re= n1+n2;
-        op="Suma"
-    }
-}
-document.problema7.opera.value= op;
-document.problema7.resul.value= re;
+        var re= 0;
+        var op= "";
+        if(n1==n2){
+            re= n1*n2;
+            op="Multiplicación"
+        }else{
+            if(n1>n2){
+                re= n1-n2;
+                op="Resta"
+            }
+            else{
+                re= n1+n2;
+                op="Suma"
+            }
+        }    
+
+        document.problema7.opera.value= op;
+        document.problema7.resul.value= re;
 }
 
 function numayor(){
     var n1= parseFloat(document.problema8.num1.value);
     var n2= parseFloat(document.problema8.num2.value);
     var n3= parseFloat(document.problema8.num3.value);
-if(n1>n2 && n1>n3){
-    document.problema8.mayor.value= n1;
-}else{
-    if(n2>n1 && n2>n3){
-        document.problema8.mayor.value= n2;
-    }
-    else{
-        if(n3>n1 && n3>n2){
-            document.problema8.mayor.value= n3;
+    if(n1>n2 && n1>n3){
+        document.problema8.mayor.value= n1;
+    }else{
+        if(n2>n1 && n2>n3){
+            document.problema8.mayor.value= n2;
         }
         else{
-            document.problema8.mayor.value= "Los Numeros son Iguales";
+            if(n3>n1 && n3>n2){
+                document.problema8.mayor.value= n3;
+            }
+            else{
+                document.problema8.mayor.value= "Los Numeros son Iguales";
+            }
         }
     }
 }
+
+function horass(){
+    var ho= parseFloat(document.problema9.horas.value);
+    var pag= parseFloat(document.problema9.pagoh.value);
+    var extrash=0;
+    var total=0;
+    var trip=0;
+    if(ho<=40){
+        document.problema9.extra.value=0;
+        total= ho*pag;
+        document.problema9.totap.value ="$"+total;
+    }
+    else{
+        if(ho<=48){
+            extrash= ho-40;
+            document.problema9.extra.value= extrash;
+            total= (extrash*pag*2)+ (40*pag);
+            document.problema9.totap.value ="$"+total;
+        }
+        else{
+            extrash= ho-40;
+            document.problema9.extra.value= extrash;
+            trip= extrash-8;
+            total= (8*pag*2)+ (40*pag)+ (trip*pag*3);
+            document.problema9.totap.value ="$"+total;
+        }
+    }
+
+}
+
+function CaUtilidad(){
+    var mes= parseFloat( document.problema10.meses.value);
+    var sala= parseFloat( document.problema10.pagom.value);
+    var total=0;
+    var ut=0;
+    if(mes<12){
+        ut= sala*0.05;
+        total= sala + ut;
+    }
+    else{
+        if(mes<24){
+            ut= sala*0.07;
+            total= sala + ut;
+        }
+        else{
+            if(mes<60){
+                ut= sala*0.1;
+                total= sala + ut;
+            }
+            else{
+                if(mes<120){
+                    ut= sala*0.15;
+                    total= sala + ut;
+                }
+                else{
+                    ut= sala*0.2;
+                    total= sala + ut;
+                }
+            }
+        }
+    }
+    document.problema10.utilidad.value="$"+ut;
+    document.problema10.totap.value="$"+total;
 }
